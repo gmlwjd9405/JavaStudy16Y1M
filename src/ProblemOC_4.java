207316427103d73b421f004ac39fb791b3f8ba79
gmlwjd9405 <gmlwjd9405@naver.com>
2016-01-28 23:17:40 +0900

    거녕재

--- src/ProblemOC_4.java
+++ src/ProblemOC_4.java
@@ -1,37 +1,6 @@
 
 public class ProblemOC_4 extends Problem {
 
-	public class Player {
-		String name; // 플레이어의 이름
-		String inputWord; // 플레이어가 입력한 단어
-
-		// 생성자
-		Player(String name) {
-			this.name = name;
-		}
-
-		Player() {
-
-		}
-
-		// 사용자로부터 단어를 입력받는 메소드
-		void sayWord(String word) {
-			this.inputWord = word;
-		}
-
-		// 끝말잇기에 성공하였는지를 판별하고 게임을 계속하는지를 판별하는 메소드
-		boolean succeed(String word) {
-			int lastIndex = word.length() - 1; // 마지막 인덱스
-			char lastChar = word.charAt(lastIndex); // 마지막 문자
-
-			if (this.inputWord.charAt(0) == lastChar) // 입력받은 문자의 첫번째 문자 == 마지막
-														// 문자
-				return true; // 끝말잇기 성공
-			else
-				return false;
-		}
-	}
-
 	@Override
 	public void run() {
 		// Scanner s = new Scanner(System.in);
@@ -52,29 +21,73 @@ public class ProblemOC_4 extends Problem {
 		String firstWord = "아버지";
 		System.out.println("시작하는 단어는 " + firstWord + "입니다. ");
 
-		int end = 0; // 종료를 판단
-		// 플레이를 반복한다.
-		while (end == 0) {
-			int firstPlay = -1; // 첫번 째 플레이를 할 때 "아버지"를 주어짐
+		int i = 0, j = 0;
+		int first = 0;
+		while (true) {
+			System.out.print(player[i].name + ">> ");
+			String playerWord = s.next();
+			player[i].sayWord(playerWord);
 
-			for (int i = 0; i; i++) {
-				System.out.print(player[i].name + ">> ");
-				String playerWord = s.next();
-				player[i].sayWord(playerWord);
-
-				if (firstPlay == -1) {
-					if (player[i].succeed(firstWord) == true) {
-						firstPlay++;
-					}
-				} else if (player[i].succeed(firstWord) == true) {
-					i++;
+			if (first == 0) {
+				if (player[0].succeed(firstWord) == true) {
+					first++;
 				} else {
-					System.out.println(player[i].name + "가 졌습니다.");
+					System.out.println(player[0].name + "가 졌습니다.");
+					break;
+				}
+
+			} else {
+				if (player[j].succeed(player[i].inputWord) == false) {
+					System.out.println(player[j].name + "가 졌습니다.");
 					break;
 				}
 			}
+
+			i++;
+			if (i == 1) {
+				j = 2;
+			} else if (i == 2) {
+				j = 0;
+			} else {// i == 3
+				i = 0;
+				j = 1;
+			}
 		}
+		// i = -1 이면 비교할 단어는 "아버지"
+		// i = 0 이면 player[1].inputWord과 비교
+		// i = 1이면 player[2].inputWord
+		// i = 2이면 player[0].inputWord
+	}
+
+}
+
+class Player {
+	String name; // 플레이어의 이름
+	String inputWord; // 플레이어가 입력한 단어
+
+	// 생성자
+	Player(String name) {
+		this.name = name;
+	}
+
+	Player() {
 
 	}
 
+	// 사용자로부터 단어를 입력받는 메소드
+	void sayWord(String word) {
+		this.inputWord = word;
+	}
+
+	// 끝말잇기에 성공하였는지를 판별하고 게임을 계속하는지를 판별하는 메소드
+	boolean succeed(String word) {
+		int lastIndex = word.length() - 1; // 마지막 인덱스
+		char lastChar = word.charAt(lastIndex); // 마지막 문자
+
+		if (this.inputWord.charAt(0) == lastChar) // 입력받은 문자의 첫번째 문자 == 마지막
+													// 문자
+			return true; // 끝말잇기 성공. 게임 계속 진행
+		else
+			return false;
+	}
 }
